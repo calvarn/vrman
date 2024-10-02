164d57db1ad1f84bd3d1ec6c96c1f330b634cac6
calvarn <2028varnadore.caleb@fcboe.org>
2024-10-02 18:29:33 -0400

    Update viseCore.sh

--- viseCore.sh
+++ viseCore.sh
@@ -273,9 +273,10 @@ fi
     	sudo -u "$VMUser" $PWD/alvr_streamer_linux/bin/alvr_dashboard > /dev/null 2>&1 &
     	pid=$!
 
-    	sleep 3
+    	sleep 5
 
     	kill $pid
+     	sleep 3
     fi
 	
 
