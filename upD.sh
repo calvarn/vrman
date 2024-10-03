f93eb3f77624fb0ebbf58ce7976edc669fe04fbc
calvarn <2028varnadore.caleb@fcboe.org>
2024-10-03 12:35:54 -0400

    Update upD.sh

--- upD.sh
+++ upD.sh
@@ -1,6 +1,6 @@
 sudo systemctl stop unattended-upgrades
 sudo systemctl disable unattended-upgrades
 rm -f -r vrman/
-sudo apt install git
+sudo apt install -y git
 git clone https://github.com/calvarn/vrman/ >/dev/null
 sudo ./vrman/viseCore.sh
